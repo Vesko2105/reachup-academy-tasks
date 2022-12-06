77cdece2d252f76760efdf832bb26bce5e5d7857
Veselin <veselin@example.com>
2022-12-06 21:06:24 +0200

    Completed Queue class

--- src/main/tasks/data_structures/Queue.java
+++ src/main/tasks/data_structures/Queue.java
@@ -1,2 +1,29 @@
-package tasks.data_structures;public class Queue {
-}
+package tasks.data_structures;
+
+public class Queue<E> {
+    private DoubleLinkedList<E> list;
+
+    public Queue() {
+        this.list = new DoubleLinkedList<>();
+    }
+
+    public void push(E element) {
+        list.addFirst(element);
+    }
+
+    public E pop() {
+        return list.removeLast();
+    }
+
+    public E top() {
+        return list.get(list.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int find(E element) {
+        return list.findLast(element);
+    }
+ }
