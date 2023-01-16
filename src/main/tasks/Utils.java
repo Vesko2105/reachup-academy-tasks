75f65f6c03960ae8cacde7bd5d3bde3626d8ccc2
Veselin <veselin@example.com>
2023-01-16 19:21:30 +0200

    Added logger supplier methods in Utils class

--- src/main/tasks/Utils.java
+++ src/main/tasks/Utils.java
@@ -3,9 +3,14 @@ package tasks;
 import tasks.basics.numbers.Factorial;
 import tasks.basics.numbers.IsPrime;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.*;
 
 public class Utils {
+
+    private static int loggerID = 0;
 
     private Utils() {
 
@@ -37,5 +42,46 @@ public class Utils {
         for (int i = 0; i < inputMatrix.length; i++) {
             inputMatrix[i] = Arrays.copyOf(matrix[i], matrix[i].length);
         }
+    }
+
+    public static Logger getConsoleLogger() {
+        Logger logger = Logger.getLogger("logger" + loggerID);
+        loggerID++;
+        logger.setUseParentHandlers(false);
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        Formatter formatter = new Formatter() {
+            @Override
+            public String format(LogRecord record) {
+                return record.getMessage() + System.lineSeparator();
+            }
+        };
+        consoleHandler.setFormatter(formatter);
+        logger.addHandler(consoleHandler);
+        return logger;
+    }
+
+    public static Logger getFileLogger(String filePath) {
+        Logger logger = Logger.getLogger("logger" + loggerID);
+        loggerID++;
+        logger.setUseParentHandlers(false);
+        FileHandler fileHandler;
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            fileHandler = new FileHandler(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Formatter formatter = new Formatter() {
+            @Override
+            public String format(LogRecord logRecord) {
+                return logRecord.getMessage() + System.lineSeparator();
+            }
+        };
+        fileHandler.setFormatter(formatter);
+        logger.addHandler(fileHandler);
+        return logger;
     }
 }
