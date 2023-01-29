1618350d88e1c87e9a25bbe72a78aec211d1e4d2
Veselin <veselin@example.com>
2023-01-29 13:16:40 +0200

    Fixes to Priority Queue class

--- src/main/tasks/data_structures/PriorityQueue.java
+++ src/main/tasks/data_structures/PriorityQueue.java
@@ -4,18 +4,25 @@ import java.util.Comparator;
 import java.util.Iterator;
 
 public class PriorityQueue<E> extends Queue<E> {
-    private Comparator<E> comparator;
+    private final Comparator<E> comparator;
 
     public PriorityQueue(Comparator<E> comparator, Order order) {
         super();
-        this.comparator = order == Order.ASCENDING ? comparator : (e1, e2) -> comparator.compare(e1, e2) * -1;
+        this.comparator = order == Order.ASCENDING ? comparator : comparator.reversed();
     }
 
     public PriorityQueue(Comparator<E> comparator) {
         this(comparator, Order.ASCENDING);
     }
 
+    public PriorityQueue() {
+        this(null, Order.ASCENDING);
+    }
+
     private int findIndex(E element) {
+        if (comparator == null) {
+            return 0;
+        }
         Iterator<E> iterator = super.list.iterator();
         int index = 0;
         while (iterator.hasNext() && comparator.compare(element, iterator.next()) <= 0) {
